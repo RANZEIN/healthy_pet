10893842286923370da62370e607052e2e4ae490
RANZEIN <ranzdhika@gmail.com>
2024-06-06 16:40:41 +0700

    first commit

---
+++ healthy_pet/lib/models/doctor_model.dart
@@ -0,0 +1,36 @@
+import 'package:healthy_pet/models/service_model.dart';
+
+class DoctorModel {
+  String name;
+  String image;
+  List<String> services;
+  int distance;
+
+  DoctorModel(
+      {required this.name,
+      required this.image,
+      required this.services,
+      required this.distance});
+}
+
+var doctors = [
+  DoctorModel(
+      name: "Dr. Stone",
+      image: "Dr. Stone.jpg",
+      services: [
+        ServiceModel.vaccine,
+        ServiceModel.surgery,
+        ServiceModel.consultation
+      ],
+      distance: 10),
+  DoctorModel(
+      name: "Dr. Vanessa",
+      image: "Dr. Vanessa.jpg",
+      services: [ServiceModel.spaAndTreatment, ServiceModel.surgery],
+      distance: 10),
+  DoctorModel(
+      name: "Dr. Ranzein",
+      image: "Dr. Vanessa.jpg",
+      services: [ServiceModel.vaccine, ServiceModel.surgery],
+      distance: 10)
+];
